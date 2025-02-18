6ad58ffe4a764275b7930166dc7bab2405508010
ShivanshTiwari01 <shivanshtiwari2014@gmail.com>
2025-02-19 00:02:23 +0530

    module finished

---
+++ 23. Node js/NPM/index.js
@@ -0,0 +1,10 @@
+import generateName from 'sillyname';
+import superheroes from 'superheroes';
+
+var sillyname = generateName();
+
+console.log(sillyname);
+
+const name = superheroes.random();
+
+console.log(name);

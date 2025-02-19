976b0e335547235b48f3c6f9f79fb7eff32f8193
ShivanshTiwari01 <shivanshtiwari2014@gmail.com>
2025-02-20 00:17:41 +0530

    module updated

--- 24. Express.js/Express Server/index.js
+++ 24. Express.js/Express Server/index.js
@@ -2,6 +2,26 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+  res.sendStatus(201);
+});
+
+app.put('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.post('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.delete('/', (req, res) => {
+  res.sendStatus(200);
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });

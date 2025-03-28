b6554a3cb25c92a2b58ba4c1ff21953da012bf9a
ShivanshTiwari01 <shivanshtiwari2014@gmail.com>
2025-03-29 00:55:21 +0530

    module updated

---
+++ 36. React.js/React Components/src/App.tsx
@@ -0,0 +1,15 @@
+import './index.css';
+import Heading from './components/Heading.tsx';
+import List from './components/List.tsx';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;

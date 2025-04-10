85540731de32dd74bd0584f7044ef6bfdf037272
ShivanshTiwari01 <shivanshtiwari2014@gmail.com>
2025-04-11 00:26:37 +0530

    module updated

--- 36. React.js/Keeper App 2/src/components/Card.tsx
+++ 36. React.js/Keeper App 2/src/components/Card.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import '../App.css';
 
-function Card() {
-  return <div>Card</div>;
-}
+const Card = (props: { title: string; content: string }) => {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
+};
 
 export default Card;
